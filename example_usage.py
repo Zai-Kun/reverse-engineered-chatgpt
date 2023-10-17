8db47bff9f763384db3360a5fa35371b0e803eb9
Zai <122824602+Zai-Kun@users.noreply.github.com>
2023-10-17 19:11:55 +0500

    Add files via upload

---
+++ example_usage.py
@@ -0,0 +1,38 @@
+import sys, asyncio
+from re_gpt import ChatGPT
+
+# colors
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RESET = '\033[0m'
+
+# it will not work on windows without this
+if sys.version_info >= (3, 8) and sys.platform.lower().startswith("win"):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+def print_chat(chat):
+    for key, message in chat.get("mapping", {}).items():
+        if "message" in message and message["message"]["content"]["parts"][0]:
+            role = message["message"]["author"]["role"]
+            content = message["message"]["content"]["parts"][0]
+            print(f"{GREEN if role == 'user' else YELLOW}{role}: {RESET}{content}\n")
+
+async def main():
+    async with ChatGPT(session_token=None) as chatgpt:
+        print_chat(await chatgpt.fetch_chat("random_string"))
+        
+        while True:
+            user_input = input(f"{GREEN}user: {RESET}")
+            reply = chatgpt.chat("random_string", user_input)
+
+            last_message = ""
+            print()
+            async for message in reply:
+                message = f"{YELLOW}assistant: {RESET}" + message
+                print(message[len(last_message) :], end="", flush=True)
+                last_message = message
+            print("\n")
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
