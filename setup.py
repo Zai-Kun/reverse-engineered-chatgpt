e1e8b11ddc6933deea18cc31fca3ac81fa7361b8
Zai <122824602+Zai-Kun@users.noreply.github.com>
2023-11-11 21:17:33 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 if __name__ == "__main__":
     setup(
         name="re_gpt",
+        author='Zai-Kun'
         packages=find_packages(),
         install_requires=["curl_cffi==0.5.9", "pycryptodome", "cryptography"],
     )
