088bb56361f2bf2fa36f62ba024524cc8afabb19
zai <122824602+Zai-Kun@users.noreply.github.com>
2023-12-09 23:35:31 +0500

    Update examples

--- examples/async_basic_example.py
+++ examples/async_basic_example.py
@@ -1,13 +1,20 @@
-from re_gpt import SyncChatGPT
+import asyncio
+import sys
+
+from re_gpt import AsyncChatGPT
 
 # consts
 session_token = "__Secure-next-auth.session-token here"
 conversation_id = None # Set it to the conversation ID if you want to continue an existing chat or None to create a new chat
 
+# If the Python version is 3.8 or higher and the platform is Windows, set the event loop policy
+if sys.version_info >= (3, 8) and sys.platform.lower().startswith("win"):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-def main():
+
+async def main():
     # Create an asynchronous ChatGPT instance using the session token from the config file
-    with SyncChatGPT(session_token=session_token) as chatgpt:
+    async with AsyncChatGPT(session_token=session_token) as chatgpt:
         # Get user input for the chat prompt
         prompt = input("Enter your prompt: ")
 
@@ -19,11 +26,12 @@ def main():
 
         last_message = ""
         # Iterate through the messages received from the chatgpt and print it
-        for message in conversation.chat("prompt"):
+        async for message in conversation.chat("prompt"):
             message = message["message"]["content"]["parts"][0]
             print(message[len(last_message) :], flush=True)
             last_message = message
 
 
 if __name__ == "__main__":
-    main()
+    # Run the asynchronous main function using asyncio.run()
+    asyncio.run(main())
