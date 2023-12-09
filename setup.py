e37a708cdf75d007a419d4487195a8bc5537916d
zai <122824602+Zai-Kun@users.noreply.github.com>
2023-12-09 23:36:21 +0500

    Update version from 2.5.0 to 2.7.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     setup(
         name="re_gpt",
         author="Zai-Kun",
-        version="2.5.0",
+        version="2.7.0",
         packages=find_packages(),
         install_requires=["curl_cffi==0.5.9", "pycryptodome", "cryptography"],
     )
