558d034a25cdf804e60dd474c23e4c1079439d6e
zai <122824602+Zai-Kun@users.noreply.github.com>
2023-12-19 19:45:32 +0500

    Update setup.py for pypi support

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import find_packages, setup
 
-if __name__ == "__main__":
-    setup(
-        name="re_gpt",
-        author="Zai-Kun",
-        version="2.8.1",
-        packages=find_packages(),
-        install_requires=["curl_cffi==0.5.9", "pycryptodome", "cryptography"],
-    )
+setup(
+    name="re_gpt",
+    version="2.8.10",
+    author="Zai-Kun",
+    description="ChatGPT web version in Python. Basically, you can use the ChatGPT API for free without any limitations, just as in the web version.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Zai-Kun/reverse-engineered-chatgpt",
+    project_urls={
+        "Bug Tracker": "https://github.com/Zai-Kun/reverse-engineered-chatgpt/issues",
+    },
+    packages=find_packages(),
+    install_requires=[
+        "curl_cffi==0.5.9",
+        "pycryptodome",
+        "cryptography",
+    ],
+)
