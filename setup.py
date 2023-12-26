97dca5cad80e8a1c9301748838463ed356ebfa0c
zai <122824602+Zai-Kun@users.noreply.github.com>
2023-12-26 18:03:27 +0500

    Update version from 2.8.10 to 2.8.11 to reupload it on pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="re_gpt",
-    version="2.8.10",
+    version="2.8.11",
     author="Zai-Kun",
     description="ChatGPT web version in Python. Basically, you can use the ChatGPT API for free without any limitations, just as in the web version.",
     long_description=open("README.md").read(),
